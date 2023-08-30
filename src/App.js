00ceca6d794b4013a56ed69de8316255047f977e
wajiha kouser <wajihawajid5610@gmail.com>
2023-08-30 14:04:20 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+
+export default function App() {
+
+  let [count, updateCounter] = useState(0);
+  let [day, changeDay] = useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className={`main ${day ? "light" : "dark"}`}>
+     
+     <h1 className='h1'>Counter App</h1><br/><br/>
+     
+     <h2 className='h2'>The value of counter is {count}</h2>
+     <br/><br/>
+     <button onClick={()=> updateCounter(count + 1)} className='btn'>Increase Counter</button>&nbsp;&nbsp;&nbsp;&nbsp;
+     <button onClick={()=> updateCounter(count - 1)} className='btn'>Decrease Counter</button> 
+     <br/><br/>
+     <button onClick={()=> changeDay(!day)} className='btn'>changeMode</button>
     </div>
   );
 }
-
-export default App;
